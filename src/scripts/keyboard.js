c6da78a35142445ae8a6346192e19d62f3355924
canberko560 <canberkvarli@gmail.com>
2021-07-14 13:22:52 -0700

    add hihat pad and stop/play functionality

--- src/scripts/keyboard.js
+++ src/scripts/keyboard.js
@@ -1,5 +1,6 @@
    document.addEventListener('DOMContentLoaded',function(event){
 
+    
        //Web Audio API
         let audioContext = new (window.AudioContext || window.webkitAudioContext)(); //base contex
         let mainGainNode = audioContext.createGain(); //gain
@@ -35,7 +36,13 @@
     
         } //key codes & note frequencies
         let volumeControl = document.querySelector("input[name='volume']");
-    
+
+        // let drumSounds = {
+        //     '219': 
+        //     // '221': 
+        //     // '220': 
+        //  };
+
         //DOM 
         
     
@@ -44,7 +51,12 @@
         let black_keys = document.querySelectorAll('black');
         let keys = document.querySelectorAll('key');
         let waveForm = document.querySelector("select[name='waveform']");
-        
+        let snare = document.getElementById('snare');
+        let kick = document.getElementById('kick');
+        let hihat = document.getElementsByClassName('hihat');
+        console.log(hihat);
+        // let hihat_source = document.getElementById('hihat');
+        let hihat_audio = hihat[0].children[0];
         //Event Listeners
 
         window.addEventListener('keydown', keyDown);
@@ -53,49 +65,19 @@
         waveForm.addEventListener('change', function(event) {
         waveForm = event.target.value;
        });    
-       
-       
+        hihat[0].addEventListener('click', function(e){
+        e.preventDefault();
+            if (hihat_audio.paused){
+                hihat_audio.play();
+
+            }else{
+                hihat_audio.pause();
+            }
+       });
+
         //FUNCTIONS
-    
-        // function createNoteTable(){
-        //     let noteFreq = [];
-    
-        //     for (let i = 0; i < 9; i++) {
-        //         noteFreq[i] = [];
-    
-        //         //octave-1
-        //         noteFreq[1]["C"] = 32.703195662574829;
-        //         noteFreq[1]["C#"] = 34.647828872109012;
-        //         noteFreq[1]["D"] = 36.708095989675945;
-        //         noteFreq[1]["D#"] = 38.890872965260113;
-        //         noteFreq[1]["E"] = 41.203444614108741;
-        //         noteFreq[1]["F"] = 43.653528929125485;
-        //         noteFreq[1]["F#"] = 46.249302838954299;
-        //         noteFreq[1]["G"] = 48.999429497718661;
-        //         noteFreq[1]["G#"] = 51.913087197493142;
-        //         noteFreq[1]["A"] = 55.000000000000000;
-        //         noteFreq[1]["A#"] = 58.270470189761239;
-        //         noteFreq[1]["B"] = 61.735412657015513;
-    
-        //         //octave-2
-        //         noteFreq[2]["C"] = 65.40639;
-        //         noteFreq[2]["C#"] = 69.29566;
-        //         noteFreq[2]["D"] = 73.41619;
-        //         noteFreq[2]["D#"] = 77.78175;
-        //         noteFreq[2]["E"] = 82.40689;
-        //         noteFreq[2]["F"] = 87.30706;
-        //         noteFreq[2]["F#"] = 92.49861;
-        //         noteFreq[2]["G"] = 97.99886;
-        //         noteFreq[2]["G#"] = 103.8262;
-        //         noteFreq[2]["A"] = 110.0000;
-        //         noteFreq[2]["A#"] = 116.5409;
-        //         noteFreq[2]["B"] = 123.4708;
-    
-        //         //Last C note
-        //         noteFreq[3]["C"] = 130.8128;
-        //         return noteFreq;
-        //     }
-        // }
+
+      
 
        function adjustVolume(e) {
            mainGainNode.gain.value = volumeControl.value
@@ -122,7 +104,7 @@
         function playNote(key) {
             const osc = audioContext.createOscillator();
             osc.frequency.setValueAtTime(noteFreq[key], audioContext.currentTime);
-            osc.type = waveForm;
+            osc.type = waveForm; //selected waveform
             oscList[key] = osc;
             oscList[key].connect(mainGainNode); //sound connected
             oscList[key].start();
