0b2c1f35557ac7412f376c409e1f5048da0e345f
canberko560 <canberkvarli@gmail.com>
2021-09-15 00:26:29 -0700

    add multiple instruments into pads

--- src/scripts/keyboard.js
+++ src/scripts/keyboard.js
@@ -47,8 +47,18 @@ document.addEventListener('DOMContentLoaded', function(event){
         let waveForm = document.querySelector("select[name='waveform']");
         let kick = document.getElementsByClassName('kick');
         let hihat = document.getElementsByClassName('hihat');
+        let bass = document.getElementsByClassName('bass');
+        let choir = document.getElementsByClassName('choir');
+        let bell = document.getElementsByClassName('bell');
+        let vocal = document.getElementsByClassName('vocal');
+
         let hihat_audio = hihat[0].children[0];
         let kick_audio = kick[0].children[0];  
+        let bass_audio = bass[0].children[0];
+        let choir_audio = choir[0].children[0];
+        let bell_audio = bell[0].children[0];
+        let vocal_audio = vocal[0].children[0];
+
         let instructions_icon = document.getElementById('instructions-icon');
         let instructions_modal = document.getElementById('instructions-modal');
         let envelope_checkbox = document.getElementById('envelope-checkbox');
@@ -99,6 +109,54 @@ document.addEventListener('DOMContentLoaded', function(event){
                 kick[0].style.color = 'black';
             }
             audioMotion.connectInput(kick_audio);
+        });
+
+        bass[0].addEventListener('click', function (e) {
+            e.preventDefault();
+            if (bass_audio.paused) {
+                bass_audio.play();
+                bass[0].style.color = 'red';
+            } else {
+                bass_audio.pause();
+                bass[0].style.color = 'black';
+            }
+            audioMotion.connectInput(bass_audio);
+        });
+
+           bell[0].addEventListener('click', function (e) {
+            e.preventDefault();
+            if (bell_audio.paused) {
+                bell_audio.play();
+                bell[0].style.color = 'red';
+            } else {
+                bell_audio.pause();
+                bell[0].style.color = 'black';
+            }
+            audioMotion.connectInput(bell_audio);
+        });
+        
+            choir[0].addEventListener('click', function (e) {
+            e.preventDefault();
+            if (choir_audio.paused) {
+                choir_audio.play();
+                choir[0].style.color = 'red';
+            } else {
+                choir_audio.pause();
+                choir[0].style.color = 'black';
+            }
+            audioMotion.connectInput(choir_audio);
+        });
+
+           vocal[0].addEventListener('click', function (e) {
+            e.preventDefault();
+            if (vocal_audio.paused) {
+                vocal_audio.play();
+                vocal[0].style.color = 'red';
+            } else {
+                vocal_audio.pause();
+                vocal[0].style.color = 'black';
+            }
+            audioMotion.connectInput(vocal_audio);
         });
         
         attackControl.addEventListener('input', function(){
@@ -159,7 +217,11 @@ document.addEventListener('DOMContentLoaded', function(event){
         };
          
             // AUDIO VISUALIZER
-        const audioMotion = new AudioMotionAnalyzer( document.getElementById('container') )
+        const audioMotion = new AudioMotionAnalyzer( document.getElementById('container'), {
+            gradient: 'prism'
+            // radial: true,
+            // spinSpeed: 20,
+        } )
 
         function playNote(key) {
             const audioCtx = audioMotion.audioCtx;
